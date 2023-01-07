8a907e4ca9db29144d4992da39fbfc8de6792571
sbienias <sandrahanrin@gmail.com>
2023-01-07 13:44:36 +0100

    restore the testing import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { testMsg, Example } from './context/auth0-context';
 
 // async function api<T>(url: string): Promise<T> {
 //   return (await fetch(url)
