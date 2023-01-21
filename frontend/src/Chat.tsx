b597d029a46b5e61bec86951ac1de62ac60fe09b
sbienias <sandrahanrin@gmail.com>
2023-01-21 12:02:02 +0100

    making it as a function

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
+import { Socket } from 'socket.io-client';
+// import { Header } from './App';
 
-class Chat extends React.Component {
-	// constructor(props) {
-	//   super(props);
-	//   this.state = {value: ''};
-	//   this.handleChange = this.handleChange.bind(this);
-	//   this.handleSubmit = this.handleSubmit.bind(this);
-	// }
-  
-	handleChange()
-	{
-		console.log("hand change");
-		// this.setState({value: event.target.value});
-	}
-	handleSubmit() {
-		console.log("aaaa");
-	//   alert('A name was submitted: ' + this.state.value);
-	//   event.preventDefault();
-	}
-  
-	render(): React.ReactNode {
-	  return(
-		<form onSubmit={this.handleSubmit}>
-		  <label>
-			Message:
-		  <input value="aaa" onChange={this.handleChange} type="text"/>
-		  </label>
-		  <input type="submit" value="Submit" />
-		</form>
-		);
-	}
-  }
 
-  export default Chat
+// function handleSubmit(value: React.FormEvent<HTMLFormElement>)
+// {
+// 	console.log("In handle submit");
+// 	console.log(value);
+// }
+
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+	const message = form.msg.value;
+    // const ageInput = event.target.elements.age; // accessing via `form.elements`
+	console.log("here");
+    console.log(message); // output: 'foo@bar.com'
+	// if (message)
+	// 	Socket.
+	alert("!!!");
+    // console.log(ageInput.value); // output: '18'
+};
+
+export function Chat({app_state, set_page} : {app_state: any, set_page: any}) {
+	return (
+		<div className="Chat">
+			{/* <Header set_page={set_page} /> */}
+			<form onSubmit={handleSubmit}>
+				<label>
+					Message:
+				<input type="text" name="msg" id="msg" placeholder="Start your chat here"/>
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	)
+}
