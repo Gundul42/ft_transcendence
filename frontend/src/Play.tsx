1fa53186619e21abb6c88f08d25a6d24f9332fdf
Gundul <sampah@mailbox.org>
2023-01-22 17:32:47 +0100

    tetris POC

--- frontend/src/Play.tsx
+++ frontend/src/Play.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Header } from './App';
+import Tetris from './components/Tetris';
+
 
 export function Play({app_state, set_page} : {app_state: any, set_page: any}) {
 	return (
 		<div className="Play">
-			<Header set_page={set_page} />
-			<h1>Here you can play against other players</h1>
+			<Tetris />
 		</div>
 	)
 }
