b6469c1834f372f760767c1f2a1c95b9ebfdda88
Gundul <sampah@mailbox.org>
2023-01-29 14:42:31 +0100

    try websockets

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,22 +1,22 @@
-// import {
-// 	OnGatewayConnection,
-// 	OnGatewayDisconnect,
-// 	OnGatewayInit,
-// 	WsResponse,
-// 	SubscribeMessage,
-// 	WebSocketGateway,
-//   } from '@nestjs/websockets';
-//   import { Server, Socket } from 'socket.io';
+ import {
+ 	OnGatewayConnection,
+ 	OnGatewayDisconnect,
+ 	OnGatewayInit,
+ 	WsResponse,
+ 	SubscribeMessage,
+ 	WebSocketGateway,
+   } from '@nestjs/websockets';
+   import { Server, Socket } from 'socket.io';
 //   import { ClientEvents } from '@shared/client/ClientEvents';
 //   import { ServerEvents } from '@shared/server/ServerEvents';
 //   import { LobbyManager } from '@app/game/lobby/lobby.manager';
 //   import { Logger, UsePipes } from '@nestjs/common';
 
-// @WebSocketGateway()
-// export class GameGateway {
-	
-// 	@SubscribeMessage("client.ping")
-// 	onPing(client: Socket): void {
-// 		client.emit("server.pong",  {message: "pong"})
-// 	}
-// }
+ @WebSocketGateway()
+ export class GameGateway {
+
+ 	@SubscribeMessage("client.ping")
+ 	onPing(client: Socket): void {
+ 		client.emit("server.pong",  {message: "pong"})
+ 	}
+ }
